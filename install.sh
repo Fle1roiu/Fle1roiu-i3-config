91aed9fa17a4b5b1cba71799e4fbd928b0f675d7
Fle1roiu <leiroiufortnite@gmail.com>
2026-02-09 15:49:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,45 +21,8 @@ echo "      FLE1ROIU SYSTEM INSTALLATION MENU"
 echo "================================================="
 echo ""
 
-# 2. Меню выбора
-echo -e "Выберите ваш дистрибутив / Select your OS:"
-echo "1) Arch Linux"
-echo "2) Fedora"
-echo "3) Ubuntu"
-echo "4) Exit / Выход"
-echo ""
-read -p ">>> Введите цифру [1-4]: " choice
-
-# 3. Логика запуска
-case $choice in
-    1)
-        if [ -f "archlinux.sh" ]; then
-            echo -e "\n[!] Запуск скрипта для Arch Linux..."
+sleep 6 
+ 
+            echo -e "\n[!] Запуск скрипта
             chmod +x archlinux.sh
             ./archlinux.sh
-        else
-            echo -e "\n[ERROR] Файл archlinux.sh не найден!"
-            sleep 2
-            ./install.sh
-        fi
-        ;;
-    2)
-        echo -e "\n[?] Скрипт для Fedora в разработке..."
-        sleep 2
-        ./install.sh
-        ;;
-    3)
-        echo -e "\n[?] Запуск скрипта для Ubuntu..."
-        chmod +x ubuntu.sh
-        ./ubuntu.sh
-        ;;
-    4)
-        echo "Выход..."
-        exit 0
-        ;;
-    *)
-        echo "Ошибка: Неверный ввод."
-        sleep 1
-        ./install.sh
-        ;;
-esac
